9abfd0a7d6304db2e454550d1f6ed6d2f5c49cab
FreemanBoss <ibrahimgbagba800@gmail.com>
2024-09-20 06:14:03 +0100

    testing

--- utils/visualization.py
+++ utils/visualization.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
     log_file_root = 'log/'
     # Custom your log files in lists, no more than len(color_list)
     log_file_list = ['log_0920_0432_57', 'log_0919_1032_57']
-    legend_text = ['SeqGAN', 'LeakGAN', 'RelGAN']
+    legend_text = ['RelGAN']
 
     color_id = 0
     data_name = 'NLL_gen'
